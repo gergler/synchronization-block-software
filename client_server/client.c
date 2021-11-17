b7d373b33298c6f3f7d680e2f3585221bf1c7fd3
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2021-11-17 21:52:14 +0700

    Update client.c

--- client_server/client.c
+++ client_server/client.c
@@ -14,16 +14,15 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in server;
 
     //Create socket
-    sock = socket(AF_INET , SOCK_DGRAM , 0);
-    if (sock < 0) {
+    if ((sock = socket(AF_INET , SOCK_DGRAM , 0)) < 0) {
         fprintf(stderr, "Error at opening stream socket\n");
         return -1;
     }
     puts("Socket created");
 
-    server.sin_addr.s_addr = inet_addr(IP);
     server.sin_family = AF_INET;
-    server.sin_port = htons( PORT );
+    server.sin_addr.s_addr = inet_addr(IP);
+    server.sin_port = htons(PORT);
 
     //Connect to remote server
     if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
@@ -35,20 +34,34 @@ int main(int argc, char *argv[]) {
     puts("Connected\n");
 
     while (1) {
-        printf("\nPlease enter command (c - config fpga, r - read reg, w - write reg, exit): ");
+        printf("Please enter command (c - config fpga, r - read reg, w - write reg, exit): ");
         scanf("%s", buffer);
+        int len, n;
+        if (sendto(sock, (char *)buffer, sizeof(buffer),0, (const struct sockaddr *) &server, sizeof(server)) < 0)
+            fprintf(stderr, "Error at sending data\n");
         if (strncmp("exit", buffer, 4) == 0) {
             printf("Client Exit\n");
             break;
         }
-        sendto(sock, (const char *)buffer, strlen(buffer),0, (const struct sockaddr *)NULL, sizeof(server));
         bzero(buffer, strlen(buffer));
-        recvfrom(sock, (char *)buffer, sizeof(buffer), 0, (struct sockaddr *)NULL, NULL);
-        puts(buffer);
+        if ((n = recvfrom(sock, (char *)buffer, sizeof(buffer), 0, (struct sockaddr *) &server, &len)) < 0)
+            fprintf(stderr, "Error at receiving data\n");
+        buffer[n] = '\0';
+        printf("%s", buffer);
+        bzero(buffer, strlen(buffer));
+        scanf("%s", buffer);
+        if (sendto(sock, (char *)buffer, sizeof(buffer),0, (const struct sockaddr *) &server, sizeof(server)) < 0)
+            fprintf(stderr, "Error at sending data\n");
+        bzero(buffer, strlen(buffer));
+
+        if ((n = recvfrom(sock, (char *)buffer, sizeof(buffer), 0, (struct sockaddr *) &server, &len)) < 0)
+            fprintf(stderr, "Error at receiving data\n");
+        buffer[n] = '\0';
+        printf("Response to command from server: %s", buffer);
         bzero(buffer, strlen(buffer));
     }
 
-    if (close(sock) == -1) {
+    if (close(sock) < 0) {
         fprintf(stderr, "Error at closing socket\n");
         return -1;
     }
