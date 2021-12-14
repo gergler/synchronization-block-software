bce47bb2ce72cbd7f32dbb180cba9992a363bfc8
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2021-12-14 17:27:47 +0700

    Update fsm_experiment.sv

--- fsm/fsm_experiment.sv
+++ fsm/fsm_experiment.sv
@@ -1,19 +1,20 @@
-module fsm_experiment(input clock, start_signal, fg_signal, wire_signal,
-					  output detonation_signal, detector_signal);
+module fsm_experiment(input clock, start_signal, fg_signal, wire_signal, detector_ready,
+					  output reg detonation_signal, output_trigger);
 
 localparam FG_DELAY = 100_000;  
-localparam DETECTOR_DELAY = 5; 
+localparam DETECTOR_READY_TIMEOUT = 5; 
 localparam TRIGGER_DELAY = 350_000; 
 
 reg[31:0] counter = 0;
 
-enum logic [2:0] {IDLE, FG_WAIT, FG, DETONATE, WIRE_TRIGGER, DETECTOR_WAIT, DETECTOR_TRIGGER} state;
+enum logic [2:0] {IDLE, FG_WAIT, FG, DETONATE, WIRE_TRIGGER, DETECTOR_WAIT, DETECTOR_FINISHED} state;
 
 always @(posedge clock) begin
 	case (state)
 		IDLE: begin
 			if (start_signal) 
 				state <= FG_WAIT;
+            {detonation_signal, output_trigger} <= '0;
 		end
 		
 		FG_WAIT: begin
@@ -32,25 +33,29 @@ always @(posedge clock) begin
 		
 		DETONATE: begin
 			detonation_signal <= 1;
+            #50;
+            detonation_signal <= 0;
 			state <= WIRE_TRIGGER;
 		end
 		
 		WIRE_TRIGGER: begin
-			if (wire_signal)
+			if (wire_signal) begin
+                output_trigger <= 1;
 				state <= DETECTOR_WAIT;
+            end
 		end
 		
 		DETECTOR_WAIT: begin
-			if (counter < DETECTOR_DELAY)
+			if ((counter < DETECTOR_READY_TIMEOUT) && (~detector_ready))
 				counter <= counter + 1;
 			else begin
-                state <= DETECTOR_TRIGGER;
+                state <= DETECTOR_FINISHED;
+                output_trigger <= 0;
                 counter <= 0;
             end
 		end
 		
-		DETECTOR_TRIGGER: begin
-			detector_signal <= 1;
+		DETECTOR_FINISHED: begin
 			state <= IDLE;
 		end
 		
