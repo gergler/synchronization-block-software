d355c870c6a11f0a1b62786f6972b1168a6a3043
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2022-02-15 00:14:31 +0700

    Update fsm_experiment.sv

--- fsm/fsm_experiment.sv
+++ fsm/fsm_experiment.sv
@@ -1,13 +1,13 @@
 `timescale 10ns/1ps
 
-module fsm_experiment(input clock, start_signal, fg_signal, wire_signal, detector_ready,
+module fsm_experiment(input clock, reset, start_signal, fg_signal, wire_signal, detector_ready,
 					  output detonation_signal, output_trigger);
 
 localparam FG_DELAY = 100_000*4;//10;  
 localparam DETECTOR_READY_TIMEOUT = 5*100; 
 localparam TRIGGER_DELAY = 350_000; 
 
-reg[31:0] counter = 0;
+reg[31:0] counter = '0;
 
 enum logic [2:0] {IDLE, FG_WAIT, FG, DETONATE, WIRE_TRIGGER, DETECTOR_WAIT, DETECTOR_FINISHED} state;
 
@@ -17,13 +17,16 @@ reg output_trigger_reg    = '0;
 assign output_trigger = output_trigger_reg;
 assign detonation_signal = detonation_signal_reg;
 
-
-always @(posedge clock) begin
+always @(posedge clock or posedge reset) begin
+	if (reset) begin
+		state <= IDLE;
+		{detonation_signal_reg, output_trigger_reg, counter} <= '0;
+	end
 	case (state)
 		IDLE: begin
 			if (start_signal) 
 				state <= FG_WAIT;
-                {detonation_signal_reg, output_trigger_reg} <= '0;
+//            {detonation_signal_reg, output_trigger_reg} <= '0;
 		end
 		
 		FG_WAIT: begin
@@ -34,34 +37,34 @@ always @(posedge clock) begin
 		FG: begin
 			if (counter < FG_DELAY)
 				counter <= counter + 1;
-            else begin
-                state <= DETONATE;
-                counter <= 0;
-            end
+         else begin
+				 state <= DETONATE;
+				 counter <= 0;
+			end
 		end
 		
 		DETONATE: begin
 			detonation_signal_reg <= 1;
-            #50;
-            detonation_signal_reg <= 0;
-            state <= WIRE_TRIGGER;
-        end
-		
+			#50;
+			detonation_signal_reg <= 0;
+			state <= WIRE_TRIGGER;
+	   end
+	
 		WIRE_TRIGGER: begin
 			if (wire_signal) begin
-                output_trigger_reg <= 1;
-				state <= DETECTOR_WAIT;
-            end
+				 output_trigger_reg <= 1;
+			state <= DETECTOR_WAIT;
+			end
 		end
 		
 		DETECTOR_WAIT: begin
 			if ((counter < DETECTOR_READY_TIMEOUT) && (~detector_ready))
 				counter <= counter + 1;
 			else begin
-                state <= DETECTOR_FINISHED;
-                output_trigger_reg <= 0;
-                counter <= 0;
-            end
+				 state <= DETECTOR_FINISHED;
+				 output_trigger_reg <= 0;
+				 counter <= 0;
+			end
 		end
 		
 		DETECTOR_FINISHED: begin
