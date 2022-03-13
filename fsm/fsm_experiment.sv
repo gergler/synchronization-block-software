f709e31df71b3980d3d7781f2133395a38d8f8f5
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2022-03-13 13:11:02 +0700

    Update fsm_experiment.sv

--- fsm/fsm_experiment.sv
+++ fsm/fsm_experiment.sv
@@ -1,63 +1,98 @@
-`timescale 10ns/1ps
+module fsm_experiment(
+		input clock, reset, start_signal, fg_signal, phase_signal, wire_signal, detector_ready, 
+		output detonation_signal, output_trigger, 
+		output [2:0] scenario_state, output int counter_out
+);
 
-module fsm_experiment(input clock, reset, start_signal, fg_signal, wire_signal, detector_ready, 
-                      input [31:0] phase_shift,
-					  output detonation_signal, output_trigger, output [2:0] scenario_state, output int counter_);
-
-localparam FG_DELAY = 100_000*4;    //10;  
+localparam FG_OPEN_DELAY = 100_000*4;    //10;  
 localparam DETECTOR_READY_TIMEOUT = 5*100; 
 localparam TRIGGER_DELAY = 350_000; 
+localparam PHASE_SHIFT = (700/5 - 1);
+localparam DETONATE_DELAY = 50;
 
 reg[31:0] counter = '0;
 
-enum logic [2:0] {IDLE, FG_WAIT, FG, DETONATE, WIRE_TRIGGER, DETECTOR_BUSY, DETECTOR_WAIT, DETECTOR_FINISHED} state;
+reg[1:0] reset_history = 0; 
+reg[1:0] start_history = 0; 
+reg[1:0] phase_history = 0; 
+reg[1:0] fg_history = 0; 
+reg[1:0] wire_history = 0;
 
 reg detonation_signal_reg = '0;
 reg output_trigger_reg    = '0;
 
+enum logic [3:0] {IDLE, FG_WAIT_OPTO, FG_WAIT_OPEN, WAIT_PHASE_FRONT, WAIT_PHASE_DELAY, DETONATE, WIRE_TRIGGER, DETECTOR_BUSY, DETECTOR_WAIT, DETECTOR_FINISHED} state;
+
 assign scenario_state = state;
-assign counter_ = counter;
+assign counter_out = counter;
 
 assign output_trigger = output_trigger_reg;
 assign detonation_signal = detonation_signal_reg;
 
 always @(posedge clock) begin
-	if (reset) begin
+	reset_history[1:0] = {reset_history[0], reset_signal};
+	start_history[1:0] = {start_history[0], start_signal}; 
+    phase_history[1:0] = {phase_history[0], phase_signal};
+    fg_history[1:0] = {fg_history[0], fg_signal};
+	wire_history[1:0] = {wire_history[0], wire_signal};
+	
+	if (reset_history == 2'b01) begin
 		state <= IDLE;
 		{detonation_signal_reg, output_trigger_reg, counter} <= '0;
 	end
     
 	case (state)
 		IDLE: begin
-			if (start_signal) 
-				state <= FG_WAIT;
-//            {detonation_signal_reg, output_trigger_reg} <= '0;
+			if (start_history == 2'b01) 
+				state <= FG_WAIT_OPTO;
 		end
 		
-		FG_WAIT: begin
-			if (fg_signal) begin
-				state <= FG;
+		FG_WAIT_OPTO: begin
+			if (fg_history == 2'b01) begin
+				state <= FG_WAIT_OPEN;
                 counter <= '0;
             end
 		end
 		
-		FG: begin
-			if (counter < FG_DELAY)
+		FG_WAIT_OPEN: begin
+			if (counter < FG_OPEN_DELAY)
 				counter <= counter + 1;
             else begin
+				state <= WAIT_PHASE_FRONT;
+				counter <= 0;
+			end
+		end
+		
+		WAIT_PHASE_FRONT: begin
+            if (phase_history == 2'b01) begin
+                state <= WAIT_PHASE_DELAY;
+				counter <= 0;
+			end
+        end
+        
+        WAIT_PHASE_DELAY: begin
+            if (counter < PHASE_SHIFT)
+				counter <= counter + 1;
+			else begin
 				state <= DETONATE;
+                counter <= 0;
 			end
 		end
 		
 		DETONATE: begin
-			detonation_signal_reg <= '1;
-			#50;
-			detonation_signal_reg <= '0;
-			state <= WIRE_TRIGGER;
+			if (counter < DETONATE_DELAY) begin
+				detonation_signal_reg <= '1;
+				counter <= counter + 1;
+			end
+			else begin
+				detonation_signal_reg <= '0;
+				state <= WIRE_TRIGGER;
+                counter <= 0;
+			end
 	   end
 	
 		WIRE_TRIGGER: begin
-			if (wire_signal) begin
+			if (wire_history == 2'b01) begin
 				output_trigger_reg <= '1;
                 state <= DETECTOR_BUSY;
 			end
