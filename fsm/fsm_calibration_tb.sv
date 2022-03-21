c31b563301e69200c89967b8cfac47c8bf898040
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2022-03-21 12:50:23 +0700

    Add files via upload

--- fsm/fsm_calibration_tb.sv
+++ fsm/fsm_calibration_tb.sv
@@ -1,16 +1,18 @@
 `timescale 1ns/10ps
+`include "fsm_tb_package.sv"
 
 module fsm_calibration_tb();
-
+	import fsm_tb_package::*;
+	
     logic clock = 0;
 	logic reset = 0;
     logic start_condition =0;
     logic fast_gate_opto = 0;
     logic fast_gate_open = 0;
+	logic phase_signal = 0;
     logic detector_ready = 1;
     logic detonator_triggered = 0;
-    logic output_trigger = 0;
-    logic phase_signal = 0; 
+    logic output_trigger = 0; 
 	logic [2:0] scenario_state;
     int counter = 0;
     
@@ -18,52 +20,33 @@ module fsm_calibration_tb();
     localparam FG_PERIOD = 20 * 1000_000; // 10ms
     localparam FG_OPENED = 100_000;    // 100us
     localparam PHASE_HALF_PERIOD = 600; 
+	localparam DETECTOR_PROLONG = 6400us;
     
     always clock = #CLOCK ~clock;
     
     always phase_signal = #PHASE_HALF_PERIOD ~phase_signal;
 	 
-	 initial begin 
-		reset = 1;
-		#100;
-		reset = 0;
-	 end 
+	initial begin
+		reset_generate(reset, 100);
+	end
      
-    //initial forever
     initial begin
         #( $urandom() % FG_PERIOD );
-        repeat(10) begin
-            fast_gate_opto = 0;
-            #(FG_PERIOD - FG_OPENED);
-            fast_gate_opto = 1;
-            #FG_OPENED;
-        end
+        repeat(10) fg_opto_generate(fast_gate_opto, FG_PERIOD, FG_OPENED);
     end
     
     always @(posedge fast_gate_opto) begin
         #9ms;
-        fast_gate_open = '1;
-        #FG_OPENED;
-        fast_gate_open = '0;
+        fg_open_generate(fast_gate_open, FG_OPENED);
     end
     
     initial begin
-        start_condition = 0;
-        #10ms;
-        start_condition = 1;
-        #20ms;
-        start_condition = 0;
-        #10ms;
-        start_condition = 1;
-        #20ms;
-        start_condition = 0;
+        repeat(2) start_generate(start_condition, 10ms, 20ms);
     end
     
     always @(posedge output_trigger) begin
         if (reset == '0) begin
-            detector_ready = 0;
-            #6400us;
-            detector_ready = 1;
+            detector_ready_generate(detector_ready, DETECTOR_PROLONG);
         end;
     end
     
@@ -73,7 +56,6 @@ module fsm_calibration_tb();
         .start_signal(start_condition), 
         .fg_signal(fast_gate_opto), 
         .phase_signal(phase_signal), 
-        //.phase_shift(phase_shift),
         
         .output_trigger(output_trigger),
 		.scenario_state(scenario_state),
