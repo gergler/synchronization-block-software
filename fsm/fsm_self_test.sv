85b61536bb35a5ce754128cbaa7b4874160d9f18
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2022-04-07 12:38:50 +0700

    Add files via upload

--- fsm/fsm_self_test.sv
+++ fsm/fsm_self_test.sv
@@ -1,39 +1,30 @@
 `timescale 1ns/10ps
 
-module fsm_self_test(input logic clock, fg_signal, input int fg_delay,
-					 output int counter_out
+module fsm_self_test(input logic clock,
+					 output [7:0] state_out
 );
 
-reg[31:0] counter = 0;
-assign counter_out = counter;
+enum logic [7:0] {IDLE, FG_WAIT_OPTO, FG_WAIT_OPEN, WAIT_PHASE_FRONT, WAIT_PHASE_DELAY, TRIGGER_PROLONG, DETECTOR_BUSY, DETECTOR_WAIT, DETECTOR_FINISHED} state;
 
-reg[1:0] fg_history = 0; 
-
-enum logic [7:0] {FG_WAIT_OPTO, FG_WAIT_OPEN} state;
+assign state_out = state;
 
 always @(posedge clock) begin
-	fg_history[1:0] = {fg_history[0], fg_signal};
-	
-	case (state)
-		FG_WAIT_OPTO: begin
-			if (fg_history == 2'b01)
-				state <= FG_WAIT_OPEN;
-			counter <= 0;
-		end
-		
-		FG_WAIT_OPEN: begin
-			if (counter < fg_delay)
-				counter <= counter + 1;
-			else begin
-				state <= FG_WAIT_OPTO;
-				counter <= 0;
-			end
-		end
-		
-		default:
-			state <= FG_WAIT_OPTO;
-	endcase			
-end    
-	
-	
+		state <= IDLE;
+		#10us;
+		state <= FG_WAIT_OPTO;
+		#10us;
+		state <= FG_WAIT_OPEN;
+		#10us;
+		state <= WAIT_PHASE_FRONT;
+		#10us;
+		state <= WAIT_PHASE_DELAY;
+		#10us;
+		state <= TRIGGER_PROLONG;
+		#10us;
+		state <= DETECTOR_BUSY;
+		#10us;
+		state <= DETECTOR_WAIT;
+		#10us;
+		state <= DETECTOR_FINISHED;
+		#10us;
 endmodule
--- fsm/fsm_self_test_tb.sv
+++ fsm/fsm_self_test_tb.sv
@@ -3,27 +3,13 @@
 module fsm_self_test_tb();
 	
     logic clock = 0;
-    logic fg_opto = 0;
-    logic fg_open = 0;
-
-	int fg_delay = 9_000_000/5;
-    int counter = 0;
-    
-    localparam CLOCK = 2.5;
-    localparam FG_PERIOD = 20 * 1000_000; // 10ms
-    localparam FG_OPENED = 100_000;    // 100us
+	logic [7:0] state;
 	
     always clock = #CLOCK ~clock;
-	fg_opto_generate fg_opto_gen(.*, .fg_period(FG_PERIOD), .fg_opened(FG_OPENED));
-	fg_open_generate fg_open_gen(.*, .fg_opened(FG_OPENED));
 	
-    
     fsm_self_test fsm_self_tst(
         .clock(clock), 
-        .fg_signal(fg_opto), 
-		.fg_delay(fg_delay),
-		
-        .counter_out(counter)
+        .state_out(state)
     );
 
     
