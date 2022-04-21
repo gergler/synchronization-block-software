2f4514991bfd1c34c3a3fb615370fa9812c4b951
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2022-04-21 17:13:10 +0700

    Delete scenario directory

--- scenario/check.sv
+++
@@ -1,7 +0,0 @@
-module check(fast_gate, phase_signal, check_condition);
-  output check_condition;
-  input fast_gate, phase_signal;
-  
-  assign check_condition = ~(fast_gate & phase_signal);
-  
-endmodule
