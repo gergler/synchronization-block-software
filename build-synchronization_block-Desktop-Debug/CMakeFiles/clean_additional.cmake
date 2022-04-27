9ccc4a94c911116593e8e23a818199c9ce5fc1f6
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2022-04-27 13:59:51 +0700

    Delete build-synchronization_block-Desktop-Debug directory

--- build-synchronization_block-Desktop-Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,7 +0,0 @@
-# Additional clean files
-
-file(REMOVE_RECURSE
-  "CMakeFiles/synchronization_block_autogen.dir/AutogenUsed.txt"
-  "CMakeFiles/synchronization_block_autogen.dir/ParseCache.txt"
-  "synchronization_block_autogen"
-)
